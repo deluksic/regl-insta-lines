1ebb5ba69a5a8f98cda651d8db3cef1761586266
David Emanuel Luksic <david.luksic@ablacon.com>
2020-04-25 20:56:13 +0200

    Fix tests, add linting and type checking

--- src/utils/__tests__/lineSegmentMesh.test.ts
+++ src/utils/__tests__/lineSegmentMesh.test.ts
@@ -8,23 +8,23 @@ describe('Test lineSegmentMesh', () => {
 
   it('returns correctly for joinCount=1', () => {
     expect(lineSegmentMesh(1)).toEqual({
-      indices: [[0, 1, 4], [3, 4, 1], [0, 4, 5], [3, 1, 2]],
-      vertices: [[0, 0, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0], [0, 1, 0], [0, 1, 1]]
+      indices: [[0, 1, 3], [0, 3, 4], [1, 0, 2], [1, 2, 6], [0, 4, 5], [1, 6, 7]],
+      vertices: [[-1, 0, 0], [1, 0, 0], [-1, -1, 0], [1, -1, 0], [-1, 1, 0], [-1, 1, 1], [1, 1, 0], [1, 1, 1]]
     });
   });
 
   it('returns correctly for joinCount=2', () => {
     expect(lineSegmentMesh(2)).toEqual({
-      indices: [[0, 1, 5], [4, 5, 1], [0, 5, 6], [0, 6, 7], [4, 1, 2], [4, 2, 3]],
-      vertices: [[0, 0, 0], [1, 0, 0], [1, 0, 0.5], [1, 0, 1], [1, 1, 0], [0, 1, 0], [0, 1, 0.5], [0, 1, 1]]
+      indices: [[0, 1, 3], [0, 3, 4], [1, 0, 2], [1, 2, 7], [0, 4, 5], [0, 5, 6], [1, 7, 8], [1, 8, 9]],
+      vertices: [[-1, 0, 0], [1, 0, 0], [-1, -1, 0], [1, -1, 0], [-1, 1, 0], [-1, 1, 0.5], [-1, 1, 1], [1, 1, 0], [1, 1, 0.5], [1, 1, 1]]
     });
   });
 
   it('returns correct number of vertices/indices for joinCount > 2', () => {
     const mesh = lineSegmentMesh(64);
-    // 64 + 64 + 4 main vertices = 132
-    expect(mesh.vertices.length).toEqual(132);
-    // 64 + 64 + 2 main triangles = 130
-    expect(mesh.indices.length).toEqual(130);
+    // 64 + 64 + 6 main vertices = 134
+    expect(mesh.vertices.length).toEqual(134);
+    // 64 + 64 + 4 main triangles = 132
+    expect(mesh.indices.length).toEqual(132);
   });
 });
