d45ce0ec94d4fdb903a78cb2ccf1c134a10e10b1
David Emanuel Luksic <david.luksic@ablacon.com>
2020-11-06 10:48:44 +0100

    Maintenance

--- src/utils/addBoundaries.ts
+++ src/utils/addBoundaries.ts
@@ -1,18 +1,18 @@
 type ArrayElement<T> = T extends (infer E)[] ? E : never;
 /**
- * When drawing a line, each segment is specified using 4 points: p0, p1, p2, p3.
+ * When drawing a line, each segment is specified using 4 consecutive points: p0, p1, p2, p3.
  * A line segment is drawn only through p1 --- p2, but p0 and p3 are important for
  * segment endings. If p0 === p1, a cap will be drawn on the start, and if p2 === p3
  * a cap will be drawn on the end. Otherwise, an appropriate "accordion" will be computed.
- * 
+ *
  * Given a line specified by points (p0, p1, ..., pn) generate boundary conditions:
  * - closed: pn, p0, p1, ..., pn, p0, p1
  * - open: p0, p0, p1, ..., pn, pn
- * 
+ *
  * Or, if fill specified, use that instead:
- * - closed fill, p0, p1, ..., pn, fill, filll
+ * - closed: fill, p0, p1, ..., pn, fill, fill
  * - open: fill, p0, p1, ..., pn, fill
- * 
+ *
  * @param points
  * @param closed
  * @param fill
